71b4dc64bed9bbaeb704430cc18f1e76c33802d8
ozgeonline <nisroc.414@hotmail.com>
2023-11-08 15:41:17 +0300

    3D test

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AnimatedLogo from '../assets/images/logo-animated.gif'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <img className='logo' src={AnimatedLogo} alt='iphone logo' />
+      
+    </div>
+  )
+}
+
+export default Loader
